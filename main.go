a5585f365817d6761f272b5f2cbe2777c44c75ce
Krishnadas <krishnadas.ns@adfolks.com>
2022-09-13 14:26:49 +0400

    Checked school functions

--- main.go
+++ main.go
@@ -10,11 +10,15 @@ import (
 func main() {
 	fmt.Printf("Entering Main Function\n")
 	s1 := student.New("Das", 26, "X", school.New("LMPS", "Vaikom"))
+	sc1 := school.New("JNLP", "New Delhi")
 
 	fmt.Printf("%s\n", s1.GetName())
 	fmt.Printf("%d\n", s1.GetAge())
 	fmt.Printf("%s\n", s1.GetSection())
 	fmt.Printf("%s\n", s1.School.GetSchoolName())
 	fmt.Printf("%s\n", s1.School.GetSchoolAddress())
+
+	fmt.Printf("%s\n", sc1.GetSchoolName())
+	fmt.Printf("%s\n", sc1.GetSchoolAddress())
 
 }
